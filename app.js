6a910f50184c0ffab16ee5a632a1ade502c76dd5
dayounglee <verdantjuly@kakao.com>
2023-06-26 16:41:56 +0900

    app.js 변경

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 const express = require('express');
-const app =
-  express();
+const app = express();
 const port = 3000;
+const commentsRouter = require('./routes/comments.route');
 
-app.listen(
-  port,
-  () => {
-    console.log(
-      port,
-      '로 서버가 열렸습니다!',
-    );
-  },
-);
+app.listen(port, () => {
+  console.log(port, '로 서버가 열렸습니다!');
+});
 
-app.get(
-  '/',
-  (
-    req,
-    res,
-  ) => {
-    res.send(
-      '메인페이지',
-    );
-  },
-);
+app.get('/', (req, res) => {
+  res.send('메인페이지');
+});
+
+app.use(express.json());
+app.use('/api', [commentsRouter]);
