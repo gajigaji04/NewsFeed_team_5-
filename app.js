bc4270654b4c176bdb3a0bc62f3740a691db4ab8
dayounglee <verdantjuly@kakao.com>
2023-06-26 16:05:18 +0900

    app.js routes 디렉토리 생성

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app =
+  express();
+const port = 3000;
+
+app.listen(
+  port,
+  () => {
+    console.log(
+      port,
+      '로 서버가 열렸습니다!',
+    );
+  },
+);
+
+app.get(
+  '/',
+  (
+    req,
+    res,
+  ) => {
+    res.send(
+      '메인페이지',
+    );
+  },
+);
