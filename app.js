dccfc98e3a9af61c7c31444750d52d15bce42b9b
dayounglee <verdantjuly@kakao.com>
2023-06-26 19:18:36 +0900

    changed skeleton

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const CookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
-const commentsRouter = require('./routes/comments.route');
+const postsRoutr = require('./routes/posts.route');
+
+app.use(express.json());
+app.use(CookieParser.json());
+
+app.use('/api', [postsRoutr]);
 
 app.listen(port, () => {
   console.log(port, '로 서버가 열렸습니다!');
@@ -10,6 +16,3 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
   res.send('메인페이지');
 });
-
-app.use(express.json());
-app.use('/api', [commentsRouter]);
---
+++ migrations/20230626082155-create-posts.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const {FOREIGNKEYS} = require('sequelize/types/query-types');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Posts', {
+      postId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'userId',
+        },
+        onDelete: 'CASCADE',
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      language: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Posts');
+  },
+};
