d59339e9a9ae4c3b14fd0122cb6f46b56261c054
Lee SeoWon <rymil9303@gmail.com>
2023-06-27 19:12:56 +0900

    게시물 CRUD 커밋

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const CookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
-
 const usersRouter = require('./routes/users.js');
+const postsRouteRouter = require('./routes/posts.route');
 
 app.use(express.json());
 app.use(CookieParser());
 
-app.use('/api', [usersRouter]);
+app.use('/api', [usersRouter, postsRouteRouter]);
 
 app.listen(port, () => {
   console.log(port, '로 서버가 열렸습니다!');
