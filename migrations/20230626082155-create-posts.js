9946d48b88488867ea07112ef3edadc90cff6cd8
Kim Joohee <syap1170@gmail.com>
2023-06-27 17:40:21 +0900

    마이그레이션 posts 수정

--- migrations/20230626082155-create-posts.js
+++ migrations/20230626082155-create-posts.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const {FOREIGNKEYS} = require('sequelize/types/query-types');
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -14,7 +12,6 @@ module.exports = {
       },
       userId: {
         allowNull: false,
-        autoIncrement: true,
         type: Sequelize.INTEGER,
         references: {
           model: 'Users',
