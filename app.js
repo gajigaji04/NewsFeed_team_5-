6c12705e62418f708b0eea7df2abbf7dd44e95c8
dayounglee <verdantjuly@kakao.com>
2023-06-27 17:09:25 +0900

    댓글 CRUD  1차 완료

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const CookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
+const commentsRouter = require('./routes/comments.route');
+
+// bodyParser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.json());
+app.use(CookieParser());
+
+app.use('/api', [commentsRouter]);
 
 app.listen(port, () => {
   console.log(port, '로 서버가 열렸습니다!');
@@ -10,3 +20,16 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
   res.send('메인페이지');
 });
+
+app.get('/detail', (req, res) => {
+  res.sendFile(__dirname + '/html/detail.html');
+});
+
+// 테이블이 없는 경우 모델 기반으로 테이블 생성
+const {sequelize} = require('./models/index.js');
+
+async function main() {
+  await sequelize.sync();
+}
+
+main();
