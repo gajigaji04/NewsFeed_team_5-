83ac7bb87379e7c1d83ff5f11d65fc237dec81a6
Kim Taejin <rlaxowls0504@gmail.com>
2023-06-27 20:47:21 +0900

    최신 커밋

--- app.js
+++ app.js
@@ -3,17 +3,12 @@ const CookieParser = require('cookie-parser');
 
 const app = express();
 const port = 3000;
-<<<<<<< HEAD
-=======
-
->>>>>>> 025f10cc711b882f4cc2a7f7bae1cceab1ae57f9
 const usersRouter = require('./routes/users.js');
 const profileRouter = require('./routes/profile.js');
 const postsRouteRouter = require('./routes/posts.route');
 
 app.use(express.json());
 app.use(CookieParser());
-
 
 app.use('/api', [usersRouter, postsRouteRouter, profileRouter]);
 
